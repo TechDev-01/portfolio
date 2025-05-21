7ce6474ef53602186539fcc5e50a454d1d7e965e
MRXjulian <floezjulian03@gmail.com>
2025-05-21 08:36:48 -0500

    Merge branch 'master' of https://github.com/TechDev-01/portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, ContactComponent],
-=======
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { ProjectsComponent } from "./components/projects/projects.component";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, ProjectsComponent],
->>>>>>> b413a6ef4153fd96b6079533c47d69eed00c42c2
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, ContactComponent, NavbarComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
